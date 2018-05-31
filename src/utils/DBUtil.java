3722e6151cc05f29498871266f872dfbde54cfdb
keita.akimoto <keita.akimoto@forumeng.co.jp>
2018-05-31 16:33:17 +0900

    Add Employee class

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -1,4 +1,5 @@
 package utils;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -8,12 +9,14 @@ public class DBUtil {
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
-            return _getEntityManagerFactory().createEntityManager();
+        return _getEntityManagerFactory().createEntityManager();
     }
+
     private static EntityManagerFactory _getEntityManagerFactory() {
-        if(emf == null){
+        if(emf == null) {
             emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         }
+
         return emf;
     }
 }
