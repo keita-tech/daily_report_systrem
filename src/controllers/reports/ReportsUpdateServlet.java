6621a636d58970f751e4dc146060a7e1f1f66d37
keita.akimoto <keita.akimoto@forumeng.co.jp>
2018-06-05 11:30:09 +0900

    Add Report create

--- src/controllers/reports/ReportsUpdateServlet.java
+++ src/controllers/reports/ReportsUpdateServlet.java
@@ -1,6 +1,7 @@
 package controllers.reports;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ReportsUpdateServlet
  */
-@WebServlet("/reports/edit")
+@WebServlet("/reports/update")
 public class ReportsUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
